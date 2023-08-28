d69a711b7562b4640041285e0383c12267f1bc03
Tyler Lartonoix <tyler@lartonoix.com>
2023-08-28 13:00:59 -0400

    Add ctrl+r fish keybind comment

--- configs/fish/conf.d/keybinds.fish
+++ configs/fish/conf.d/keybinds.fish
@@ -1,3 +1,4 @@
 function fish_user_key_bindings
+	# Use ctrl+r to resume the first backgrounded process
 	bind \cr 'fg >/dev/null 2>&1 || echo -e "\nNo background jobs"; commandline -f repaint'
 end
