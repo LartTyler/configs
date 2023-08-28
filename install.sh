d6fda382a2f8cca25205efdb19bee664e0e6dd1a
Tyler Lartonoix <tyler@lartonoix.com>
2023-08-28 13:06:40 -0400

    Update rust-analyzer installation method

--- install.sh
+++ install.sh
@@ -93,10 +93,7 @@ make -C /usr/share/doc/git/contrib/credential/libsecret
 # Install rust-lang + tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -yq --no-modify-path
 
-mkdir -p ~/.local/bin
-curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
-chmod +x ~/.local/bin/rust-analyzer
-
+~/.cargo/bin/rustup component add rust-analyzer
 ~/.cargo/bin/cargo install proximity-sort
 
 runuser -u "$user_name" -c './do_configs.sh'
